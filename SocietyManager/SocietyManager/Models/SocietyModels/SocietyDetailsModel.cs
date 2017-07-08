5ea9b293cc68bc37e9127702fb80ef1a69926098
RushikeshMusale <rushikeshmusale@gmail.com>
2017-07-08 17:10:02 +0530

    Added SocietyDetail View model - initial

---
+++ SocietyManager/SocietyManager/Models/SocietyModels/SocietyDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocietyManager.Models.SocietyModels
+{
+    public class SocietyDetailsModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Builder { get; set; }
+        public string AccountNumber { get; set; }
+        public int NumberOfFlats { get; set; }
+        
+    }
+}
