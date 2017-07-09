2d0c0bb17fef9cfe70714961d9fef6503878bb70
RushikeshMusale <rushikeshmusale@gmail.com>
2017-07-09 12:18:51 +0530

    Added RegisterSociety Method & applied Admin Authorization

--- SocietyManager/SocietyManager/Controllers/SocietyControllers/SocietyController.cs
+++ SocietyManager/SocietyManager/Controllers/SocietyControllers/SocietyController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SocietyManager.Data;
+using SocietyManager.Models.SocietyModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,12 +8,32 @@ using System.Web.Mvc;
 
 namespace SocietyManager.Controllers.SocietyControllers
 {
+    [Authorize(Roles ="Admin")]
     public class SocietyController : Controller
     {
+        SocietyMaintenanceEntities1 db = new SocietyMaintenanceEntities1();
         // GET: Society
         public ActionResult Index()
         {
             return View();
         }
+
+
+        
+        public ActionResult RegisterSociety()
+        {
+            SocietyDetailsModel sd = new SocietyDetailsModel();
+            return View(sd);
+                
+        }
+
+        [HttpPost]
+        public ActionResult RegisterSociety(SocietyDetailsModel model)
+        {
+            //Method to save image
+            return View();
+
+        }
+
     }
 }
