5dd639bd3d788bba41751a0e4d7284651311c0d0
RushikeshMusale <rushikeshmusale@gmail.com>
2017-08-15 17:11:34 +0530

    FlatDetails Model added

---
+++ SocietyManager/SocietyManager/Models/SocietyModels/FlatDetailsModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SocietyManager.Models.SocietyModels
+{
+    public class FlatDetailsModel
+    {
+        public int FlatId { get; set; }
+        [Required]
+        public string FlatNumber { get; set; }
+        [Required]
+        [Range(0,5,ErrorMessage ="Please Enter between 1 & 5")]
+        public int BHK { get; set; }
+
+        [Range(100,5000,ErrorMessage ="Please enter area between 100 & 5000")]
+        public double Area { get; set; }
+        public bool IsRented { get; set; }
+    }
+}
